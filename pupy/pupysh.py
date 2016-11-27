d62d4da0e9688afaf575a71e6627da5a89926f55
Oleksii Shevchuk <alxchk@gmail.com>
2016-11-27 13:49:19 +0200

    Integrate DNSCNC

--- pupy/pupysh.py
+++ pupy/pupysh.py
@@ -16,8 +16,6 @@
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 # --------------------------------------------------------------
 
-import pupylib.PupyServer
-import pupylib.PupyCmd
 try:
     import pupylib.PupySignalHandler
 except:
@@ -30,22 +28,36 @@ import os
 import os.path
 import network.conf
 
-__author__='Nicolas VERDIER'
-__version__='v1.3'
-__date__='Jun 17 2016'
+from pupylib import PupyServer
+from pupylib import PupyDnsCnc
+from pupylib import PupyCmdThread
+from pupylib import __version__
+
+from network.lib.igd import IGDClient, UPNPError
 
 def print_version():
     print("Pupy - %s"%(__version__))
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(prog='pupysh', description="Pupy console")
-    parser.add_argument('--log-lvl', '--lvl', help="change log verbosity", dest="loglevel", choices=["DEBUG","INFO","WARNING","ERROR"], default="WARNING")
-    parser.add_argument('--version', help="print version and exit", action='store_true')
-    parser.add_argument('-t', '--transport', choices=[x for x in network.conf.transports.iterkeys()], help="change the transport ! :-)")
-    parser.add_argument('--ta', '--transport-args', dest='transport_args', help="... --transport-args 'OPTION1=value OPTION2=val ...' ...")
-    parser.add_argument('--port', '-p', help="change the listening port", type=int)
+    parser.add_argument(
+        '--log-lvl', '--lvl',
+        help='change log verbosity', dest='loglevel',
+        choices=['DEBUG','INFO','WARNING','ERROR'],
+        default='WARNING')
+    parser.add_argument('--version', help='print version and exit', action='store_true')
+    parser.add_argument(
+        '-t', '--transport',
+        choices=[x for x in network.conf.transports.iterkeys()],
+        help='change the transport ! :-)')
+    parser.add_argument(
+        '--ta', '--transport-args', dest='transport_args',
+        help='... --transport-args " OPTION1=value OPTION2=val ..." ...')
+    parser.add_argument('--port', '-p', help='change the listening port', type=int)
+    parser.add_argument('--dns', '-d', help='enable dnscnc server. FDQN:port', type=str)
+    parser.add_argument('--external-ip', '-e', help='setup external ip address', type=str)
     parser.add_argument('--workdir', help='Set Workdir (Default = current workdir)')
-    args=parser.parse_args()
+    args = parser.parse_args()
 
     if args.workdir:
        os.chdir(args.workdir)
@@ -57,24 +69,43 @@ if __name__=="__main__":
     logging.basicConfig(format='%(asctime)-15s - %(levelname)-5s - %(message)s')
     logging.getLogger().setLevel(args.loglevel)
 
-    pupyServer=pupylib.PupyServer.PupyServer(args.transport, args.transport_args, port=args.port)
     try:
-        import __builtin__ as builtins
-    except ImportError:
-        import builtins
-    builtins.glob_pupyServer=pupyServer # dirty ninja trick for this particular case avoiding to touch rpyc source code
-    pcmd=pupylib.PupyCmd.PupyCmd(pupyServer)
+        igd = IGDClient()
+    except UPNPError as e:
+        pass
+
+    pupyServer = PupyServer(
+        args.transport,
+        args.transport_args,
+        port=args.port,
+        igd=igd,
+    )
+
+    pupyDnsCnc = None
+    if args.dns:
+        if ':' in args.dns:
+            fdqn, dnsport = args.dns.split(':')
+        else:
+            fdqn = args.dns.strip()
+            dnsport = 5454
+
+        pupyDnsCnc = PupyDnsCnc(
+            fdqn,
+            igd=igd,
+            port=dnsport,
+            connect_host=args.external_ip,
+            connect_port=args.port or 443,
+            connect_transport=args.transport or 'ssl',
+        )
+
+    pupycmd = PupyCmdThread(pupyServer, pupyDnsCnc)
+
     pupyServer.start()
-    active = True
+    pupycmd.start()
 
-    while active:
-        try:
-            pcmd.cmdloop()
-            active = False
-        except Exception as e:
-            print(traceback.format_exc())
-            time.sleep(0.1) #to avoid flood in case of exceptions in loop
-            pcmd.intro=''
+    try:
+        pupyServer.finished.wait()
+    except KeyboardInterrupt:
+        pass
 
-    pupyServer.stop()
-    pupyServer.join()
+    pupycmd.stop()
